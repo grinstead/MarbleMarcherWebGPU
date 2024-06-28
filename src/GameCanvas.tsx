4a6cfdbe003f8adf2b79c9849c6c381c3fd72075
Joe Grinstead <npm@mailforce.net>
2024-06-28 10:20:32 -0400

    Render a sphere with normals
    I think it's more fun for me if I don't port the original code directly, but instead build it up.
    
    One that that happened was that he added, unusually, a way to get the nearest point in the scene. However, he doesn't use that info elsewhere, so I wanted to experiment with using it (for something like getting the normal calculation)

--- src/GameCanvas.tsx
+++ src/GameCanvas.tsx
@@ -48,24 +48,56 @@ fn inCamera(position: vec4f) -> vec3f {
 
 const CIRCLE_ORIGIN = vec3f(0, 0, 10);
 const CIRCLE_RADIUS = 1.0f;
+const MAX_STEPS = 100;
+const EPSILON = 1e-5;
 
 struct RayMarchResult {
-  closest: vec3f,
-  distance: f32,
-  steps: i32,
+  // the distance to the scene
+  scenePoint: vec3f,
+  // the amount of remaining distance
+  headroom: f32,
+  // The position we ended the ray march in
+  endPoint: vec3f,
+  steps: f32,
+}
+
+fn ed_circle(point: vec3f) -> f32 {
+  return length(point - CIRCLE_ORIGIN) - CIRCLE_RADIUS;
+}
+
+fn estimateHeadroom(point: vec3f) -> f32 {
+  return ed_circle(point);
+}
+
+// http://www.iquilezles.org/www/articles/normalsSDF/normalsSDF.htm
+fn calcNormal(point: vec3f) -> vec3f {
+  let offset = .5 * EPSILON;
+  let k = vec2f(1, -1);
+	return normalize(
+    k.xyy * estimateHeadroom(point + k.xyy * offset) +
+		k.yyx * estimateHeadroom(point + k.yyx * offset) +
+		k.yxy * estimateHeadroom(point + k.yxy * offset) +
+		k.xxx * estimateHeadroom(point + k.xxx * offset)
+  );
 }
 
 fn rayMarch(start: vec3f, direction: vec3f) -> RayMarchResult {
   var steps = 0;
   var position = start;
+  var distance = 0.;
   loop {
-    let distance = length(position - CIRCLE_ORIGIN) - CIRCLE_RADIUS;
-  
-    if (distance < .01 || steps >= 20) {
-      return RayMarchResult(position, distance, steps);
-    }
+    let headroom = estimateHeadroom(position);
+    distance += headroom;
+    position += headroom * direction;
 
-    position += distance * direction;
+    if (headroom < EPSILON || steps >= MAX_STEPS) {
+      return RayMarchResult(
+        CIRCLE_ORIGIN + CIRCLE_RADIUS * normalize(position - CIRCLE_ORIGIN),
+        headroom,
+        position,
+        f32(steps)
+      );
+    }
 
     continuing {
       steps++;
@@ -73,14 +105,20 @@ fn rayMarch(start: vec3f, direction: vec3f) -> RayMarchResult {
   }
 }
 
+
 @fragment
 fn fragment_main(@location(0) fragUV: vec2f) -> @location(0) vec4f {
   var position = inCamera(vec4f(fragUV - 0.5, -1, 1));
   var dir = normalize(inCamera(vec4f(0, 0, 0, 1)) - position);
 
-  let result = rayMarch(position, dir);
+  let march = rayMarch(position, dir);
+  if (march.headroom <= EPSILON) {
+    let norm = calcNormal(march.scenePoint);
+    return vec4f(norm, 1.);
+  }
 
-  return select(vec4f(.1, 0, 0, 0), vec4f(0, 1. - f32(result.steps) * .01, 0, 0), result.distance < 1);
+
+  return vec4f(.1, 0, 0, 1);
 }
     
     `;
