0c4d891bc056398fd779747fd520a89d0f0098e0
Joe Grinstead <npm@mailforce.net>
2024-07-09 12:07:47 -0400

    Fix ported shader compiler errors, switch to shader.

--- src/Graphics.tsx
+++ src/Graphics.tsx
@@ -7,367 +7,24 @@ import {
   xyzArray,
 } from "@grinstead/ambush";
 import { GameStore } from "./GameStore.ts";
-import { createSignal, onCleanup, onMount } from "solid-js";
-
-const RENDER_QUAD = `
-struct VertexOutput {
-  @builtin(position) position: vec4f,
-  @location(0) fragUV: vec2f,
-}
-
-// just renders 4 points that cover the canvas, all work done in fragment shader
-@vertex
-fn vertex_main(@builtin(vertex_index) index: u32) -> VertexOutput {
-  const fragPoints = array(
-    vec2f(0, 0),
-    vec2f(1, 0),
-    vec2f(0, 1),
-    vec2f(1, 1),
-  );
-
-  let uv = fragPoints[index];
-
-  return VertexOutput(
-    vec4f(2 * uv - 1, 0, 1),
-    uv,
-  );
-}`;
+import { frag } from "./frag.ts";
 
 export type GameCanvasProps = {
   store: GameStore;
 };
 
 export function Graphics(props: GameCanvasProps) {
-  const MyTestShaderCode = `
-${RENDER_QUAD}
-
-// matrix expressed in meters, implictly encodes focal length
-// because when rendering we shoot out rays from the "sensor"
-// which is the square from (-.5, -.5, -1)x(.5, .5, -1), shot
-// at the origin, where both of those values have been multiplied
-// by the camera
-@group(0) @binding(0) var<uniform> camera: mat4x4f;
-@group(0) @binding(1) var<uniform> iResolution: vec2f;
-
-@group(1) @binding(0) var<uniform> iFracScale: f32;
-@group(1) @binding(1) var<uniform> iFracAng1: f32;
-@group(1) @binding(2) var<uniform> iFracAng2: f32;
-@group(1) @binding(3) var<uniform> iFracShift: vec3f;
-@group(1) @binding(4) var<uniform> iFracCol: vec3f;
-@group(1) @binding(5) var<uniform> iMarbleRad: f32;
-
-@group(2) @binding(0) var<uniform> iMarblePos: vec3f;
-
-fn inCamera(position: vec4f) -> vec3f {
-  let transformed = camera * position;
-  return transformed.xyz / transformed.w;
-}
-
-const AMBIENT_OCCLUSION_COLOR_DELTA = vec3f(0.7);
-const AMBIENT_OCCLUSION_STRENGTH = 0.008;
-const BACKGROUND_COLOR = vec4f(0.6, 0.8, 1.0, 1.0);
-const CIRCLE_ORIGIN = vec3f(0, 0, 40);
-const CIRCLE_RADIUS = 1.0f;
-const EPSILON = 1e-5;
-const FOCAL_DIST = 1.73205080757;
-const FRACTAL_LEVELS = 16;
-const LIGHT_DIRECTION = vec3f(-0.36, 0.8, 0.48);
-const LIGHT_COLOR = vec3f(1.0, 0.95, 0.8);
-const MAX_DISTANCE = 30.0;
-const MAX_STEPS = 1000;
-const SHADOW_DARKNESS = 0.7;
-const SHADOW_SHARPNESS = 10.0;
-const SPECULAR_HIGHLIGHT = 40;
-const SPECULAR_MULT = 0.25;
-const SUN_SHARPNESS = 2.0;
-const SUN_SIZE = 0.004;
-const VIGNETTE_STRENGTH = 0.5;
-
-const ID_VOID = 0;
-const ID_FRACTAL = 1;
-const ID_MARBLE = 2;
-const ID_FLAG = 3;
-
-struct DistanceAndShape {
-  headroom: f32,
-  shape: u32,
-}
-
-struct RayMarchResult {
-  // The position we ended the ray march in
-  endPoint: vec4f,
-  // the amount of remaining distance
-  headroom: f32,
-  minHeadroom: f32,
-  distance: f32,
-  steps: f32,
-  shape: u32,
-}
-
-fn rotX(p: vec4f, a: f32) -> vec4f {
-  let c = cos(a);
-  let s = sin(a);
-  return vec4f(p.x, c * p.y + s * p.z, c * p.z - s * p.y, p.w);
-}
-
-fn rotZ(p: vec4f, a: f32) -> vec4f {
-  let c = cos(a);
-  let s = sin(a);
-  return vec4f(c * p.x + s * p.y, c * p.y - s * p.x, p.zw);
-}
-
-fn max3(a: f32, b: f32, c: f32) -> f32 {
-  return max(max(a, b), c);
-}
-
-fn max4(a: f32, b: f32, c: f32, d: f32) -> f32 {
-  return max(max(a, b), max(c, d));
-}
-
-/////////////////////////////////////////////////////////////////////////////
-// Geometry
-/////////////////////////////////////////////////////////////////////////////
-
-fn mengerFold(point: vec4f) -> vec4f {
-  var p = point;
-  
-  var a = min(p.x - p.y, 0.0);
-  p.x -= a;
-  p.y += a;
-
-  a = min(p.x - p.z, 0.0);
-  p.x -= a;
-  p.z += a;
-
-  a = min(p.y - p.z, 0.0);
-  p.y -= a;
-  p.z += a;
-
-  return p;
-}
-
-fn de_sphere(p: vec3f, r: f32) -> f32 {
-	return length(p) - r;
-}
-
-fn de_box(p: vec4f, sides: vec3f) -> f32 {
-	let a = abs(p.xyz) - sides;
-	return (min(max3(a.x, a.y, a.z), 0.0) + length(max(a, vec3f()))) / p.w;
-}
-
-fn de_tetrahedron(p: vec3f, r: f32) -> f32 {
-	let md = max4(
-    -p.x - p.y - p.z,
-     p.x + p.y - p.z,
-		-p.x + p.y + p.z,
-     p.x - p.y + p.z
-  );
-	return (md - r) / sqrt(3.0);
-}
-
-fn de_capsule(p: vec3f, h: f32, r: f32) -> f32 {
-	return length(p - vec3f(0, clamp(p.y, -h, h), 0)) - r;
-}
-
-fn de_fractal(point: vec4f) -> f32 {
-  var p = point;
-  for (var i = 0; i < FRACTAL_LEVELS; i++) {
-    p = vec4f(abs(p.xyz), p.w);
-    p = rotZ(p, iFracAng1);
-    p = mengerFold(p);
-    p = rotX(p, iFracAng2);
-    p *= iFracScale;
-    p += vec4f(iFracShift, 0.);
-  }
-
-  return de_box(p, vec3f(6.));
-}
-
-fn col_fractal(point: vec4f) -> vec4f {
-  var orbit = vec3f();
-  var p = point;
-  for (var i = 0; i < FRACTAL_LEVELS; i++) {
-    p = vec4f(abs(p.xyz), p.w);
-    p = rotZ(p, iFracAng1);
-    p = mengerFold(p);
-    p = rotX(p, iFracAng2);
-    p *= iFracScale;
-    p += vec4f(iFracShift, 0.);
-    orbit = max(orbit, p.xyz * iFracCol);
-  }
-
-  return vec4f(orbit, 1);
-}
-
-fn estimateHeadroom(point: vec4f) -> DistanceAndShape {
-  var result = DistanceAndShape(
-    de_fractal(point),
-    ID_FRACTAL,
-  );
-  
-  let dist = de_sphere(point.xyz - iMarblePos, iMarbleRad);
-  if (dist <= result.headroom) {
-    result.headroom = dist;
-    result.shape = ID_MARBLE;
-  }
-
-  return result;
-}
-
-/////////////////////////////////////////////////////////////////////////////
-// Main Code
-/////////////////////////////////////////////////////////////////////////////
-
-// http://www.iquilezles.org/www/articles/normalsSDF/normalsSDF.htm
-fn calcNormal(point: vec4f, offset: f32) -> vec3f {
-  let k = vec3f(1, -1, 0);
-	return normalize(
-    k.xyy * estimateHeadroom(point + k.xyyz * offset).headroom +
-		k.yyx * estimateHeadroom(point + k.yyxz * offset).headroom +
-		k.yxy * estimateHeadroom(point + k.yxyz * offset).headroom +
-		k.xxx * estimateHeadroom(point + k.xxxz * offset).headroom
-  );
-}
-
-fn rayMarch(start: vec4f, direction: vec3f, sharpness: f32) -> RayMarchResult {
-  let FOVperPixel = 1.0 / max(iResolution.x, 900.0);
-
-  var steps = 0;
-  var position = start;
-  var distance = 0.;
-  var minHeadroom = 1.;
-  loop {
-    let adjustedMin = max(FOVperPixel * distance, EPSILON);
- 
-    let estimate = estimateHeadroom(position);
-
-    if (estimate.headroom < adjustedMin || distance > MAX_DISTANCE || steps >= MAX_STEPS) {
-      return RayMarchResult(
-        position,
-        estimate.headroom,
-        minHeadroom,
-        distance,
-        f32(steps) + select(
-          0., 
-          estimate.headroom / adjustedMin,
-          estimate.headroom < adjustedMin
-        ),
-        select(estimate.shape, ID_VOID, distance > MAX_DISTANCE || steps >= MAX_STEPS)
-      );
-    }
-
-    continuing {
-      steps++;
-      distance += estimate.headroom;
-      position += vec4f(estimate.headroom * direction, 0);
-      minHeadroom = min(minHeadroom, sharpness * estimate.headroom / distance);
-    }
-  }
-}
-
-fn fractalSurface(dir: vec3f, march: RayMarchResult) -> vec3f {
-  let FOVperPixel = 1.0 / max(iResolution.x, 900.0);
-  let minDist = max(FOVperPixel * march.distance, EPSILON);
-  let norm = calcNormal(march.endPoint, minDist * 0.5);
-
-  // find closest surface point, without this we get weird coloring artifacts
-  let endPoint = march.endPoint.xyz - norm * march.headroom;
-  var color = vec3f(0);
-
-  var materialColor: vec3f;
-  if (march.shape == ID_MARBLE) {
-    materialColor = vec3f(0);
-  } else {
-    materialColor = saturate(
-      col_fractal(vec4f(endPoint, 1)).xyz
-    );
-  }
-
-  var k = 1.;
-
-  // shadows
-  let lightMarch = rayMarch(
-    vec4f(endPoint + norm * EPSILON * 100., 1.), 
-    LIGHT_DIRECTION, 
-    SHADOW_SHARPNESS
-  );
-  k = lightMarch.minHeadroom * min(lightMarch.distance, 1.0);
-
-  let reflected = dir - 2. * dot(dir, norm) * norm;
-  var specular = max(dot(reflected, LIGHT_DIRECTION), 0.0);
-  specular = pow(specular, SPECULAR_HIGHLIGHT);
-  color += specular * LIGHT_COLOR * (k * SPECULAR_MULT);
-
-  // diffuse lighting
-  k = min(k, SHADOW_DARKNESS * 0.5 * (dot(norm, LIGHT_DIRECTION) - 1.) + 1.);
-
-  k = max(k, 1.0 - SHADOW_DARKNESS);
-
-  color += materialColor * LIGHT_COLOR * k;
-
-  // let a = 1.0 / (1.0 + march.steps * AMBIENT_OCCLUSION_STRENGTH);
-  // color += (1.0 - a) * AMBIENT_OCCLUSION_COLOR_DELTA;
-
-  return saturate(color);
-}
-
-@fragment
-fn fragment_main(@location(0) fragUV: vec2f) -> @location(0) vec4f {
-  let FOVperPixel = 1.0 / max(iResolution.x, 900.0);
-
-  var uv = 2. * fragUV - 1.;
-  uv.x *= iResolution.x / iResolution.y;
-
-  var dir = (camera * normalize(vec4f(uv.x, uv.y, -FOCAL_DIST, 0.0))).xyz;
-  var position = camera[3];
-
-  let march = rayMarch(position, dir, 1.0);
-  if (march.shape == ID_VOID) {
-    // The ray did not hit the target
-    var color = BACKGROUND_COLOR;
-
-    let vignette = 1.0 - VIGNETTE_STRENGTH * length(fragUV * 0.5);
-    color = vec4f(color.xyz * vignette, 1);
-  
-    // "spec" for specular
-    var sunSpec = dot(dir, LIGHT_DIRECTION) - 1.0 + SUN_SIZE;
-    sunSpec = min(exp(sunSpec * SUN_SHARPNESS / SUN_SIZE), 1.);
-    color += vec4f(LIGHT_COLOR * sunSpec, 0);
-  
-    return color;
-  } else if (march.shape == ID_MARBLE) {
-    // Refract when entering the marble, and exiting
-    let n = normalize(march.endPoint.xyz - iMarblePos);
-    let q = refract(dir, n, 1.0 / 1.5);
-    let exitPoint = march.endPoint.xyz + (dot(q, n) * 2.0 * iMarbleRad) * q;
-    let exitN = normalize(exitPoint - iMarblePos);
-    let exitQ = q;// (dot(q, dir) * 2.0) * q - dir;
-
-    let exitStep = vec4f(exitPoint + exitN * EPSILON * 10.0, 1.0);
-    let refrCol = fractalSurface(
-      exitQ,
-      rayMarch(exitStep, exitQ, 1.0)
-    );
-
-    return vec4f(refrCol, 1);
-  } else {
-    return vec4f(fractalSurface(dir, march), 1);
-  }
-}
-    
-    `;
-
   return (
     <>
       <RenderPipeline
         label="Test Shader"
-        code={MyTestShaderCode}
+        code={frag()}
         vertexMain="vertex_main"
-        fragmentMain="fragment_main"
+        fragmentMain="main"
         draw={4}
       />
       <BufferBinding
-        label="camera"
+        label="iMat"
         group={0}
         id={0}
         value={props.store.cameraMatrix}
@@ -375,14 +32,14 @@ fn fragment_main(@location(0) fragUV: vec2f) -> @location(0) vec4f {
       <VectorBinding label="iResolution" group={0} id={1} value={[640, 480]} />
       <ScalarBinding
         label="iFracScale"
-        group={1}
-        id={0}
+        group={0}
+        id={3}
         value={props.store.level.scale}
       />
       <ScalarBinding
         label="iFracAng1"
-        group={1}
-        id={1}
+        group={0}
+        id={4}
         value={(() => {
           const anim = props.store.level.animation.x;
 
@@ -394,8 +51,8 @@ fn fragment_main(@location(0) fragUV: vec2f) -> @location(0) vec4f {
       />
       <ScalarBinding
         label="iFracAng2"
-        group={1}
-        id={2}
+        group={0}
+        id={5}
         value={(() => {
           const anim = props.store.level.animation.y;
 
@@ -407,8 +64,8 @@ fn fragment_main(@location(0) fragUV: vec2f) -> @location(0) vec4f {
       />
       <VectorBinding
         label="iFracShift"
-        group={1}
-        id={3}
+        group={0}
+        id={6}
         value={(() => {
           const anim = props.store.level.animation.z;
           const offset = xyzArray(props.store.level.offset);
@@ -422,22 +79,37 @@ fn fragment_main(@location(0) fragUV: vec2f) -> @location(0) vec4f {
       />
       <VectorBinding
         label="iFracCol"
-        group={1}
-        id={4}
+        group={0}
+        id={7}
         value={rgbArray(props.store.level.color)}
-      />
-      <ScalarBinding
-        label="iMarbleRad"
-        group={1}
-        id={5}
-        value={props.store.level.marbleRadius}
       />
       <VectorBinding
         label="iMarblePos"
-        group={2}
-        id={0}
+        group={0}
+        id={8}
         value={xyzArray(props.store.level.marblePosition)}
       />
+      <ScalarBinding
+        label="iMarbleRad"
+        group={0}
+        id={9}
+        value={props.store.level.marbleRadius}
+      />
+      <ScalarBinding
+        label="iFlagScale"
+        group={0}
+        id={10}
+        value={
+          (props.store.level.isPlanet ? -1 : 1) * props.store.level.marbleRadius
+        }
+      />
+      <VectorBinding
+        label="iFlagPos"
+        group={0}
+        id={11}
+        value={xyzArray(props.store.level.flagPosition)}
+      />
+      <ScalarBinding label="iExposure" group={0} id={12} value={1} />
     </>
   );
 }
--- src/frag.ts
+++ src/frag.ts
@@ -1,5 +1,31 @@
+const RENDER_QUAD = `
+struct VertexOutput {
+  @builtin(position) position: vec4f,
+  @location(0) fragUV: vec2f,
+}
+
+// just renders 4 points that cover the canvas, all work done in fragment shader
+@vertex
+fn vertex_main(@builtin(vertex_index) index: u32) -> VertexOutput {
+  const fragPoints = array(
+    vec2f(0, 0),
+    vec2f(1, 0),
+    vec2f(0, 1),
+    vec2f(1, 1),
+  );
+
+  let uv = fragPoints[index];
+
+  return VertexOutput(
+    vec4f(2 * uv - 1, 0, 1),
+    uv,
+  );
+}`;
+
 export function frag() {
   return `
+${RENDER_QUAD}
+
 @group(0) @binding(0) var<uniform> iMat: mat4x4<f32>;
 @group(0) @binding(1) var<uniform> iResolution: vec2<f32>;
 @group(0) @binding(2) var<uniform> iDebug: vec3<f32>;
@@ -13,17 +39,12 @@ export function frag() {
 @group(0) @binding(10) var<uniform> iFlagScale: f32;
 @group(0) @binding(11) var<uniform> iFlagPos: vec3<f32>;
 @group(0) @binding(12) var<uniform> iExposure: f32;
-@group(0) @binding(0) var<uniform> uniforms: Uniforms;
 
 const AMBIENT_OCCLUSION_COLOR_DELTA: vec3<f32> = vec3<f32>(0.7, 0.7, 0.7);
 const AMBIENT_OCCLUSION_STRENGTH: f32 = 0.008;
 const ANTIALIASING_SAMPLES: u32 = 1;
 const BACKGROUND_COLOR: vec3<f32> = vec3<f32>(0.6, 0.8, 1.0);
-const DIFFUSE_ENABLED: u32 = 0;
-const DIFFUSE_ENHANCED_ENABLED: u32 = 1;
-const FILTERING_ENABLE: u32 = 0;
 const FOCAL_DIST: f32 = 1.73205080757;
-const FOG_ENABLED: u32 = 0;
 const FRACTAL_ITER: u32 = 16;
 const LIGHT_COLOR: vec3<f32> = vec3<f32>(1.0, 0.95, 0.8);
 const LIGHT_DIRECTION: vec3<f32> = vec3<f32>(-0.36, 0.8, 0.48);
@@ -31,12 +52,10 @@ const MAX_DIST: f32 = 30.0;
 const MAX_MARCHES: u32 = 1000;
 const MIN_DIST: f32 = 1e-5;
 const PI: f32 = 3.14159265358979;
-const SHADOWS_ENABLED: u32 = 1;
 const SHADOW_DARKNESS: f32 = 0.7;
 const SHADOW_SHARPNESS: f32 = 10.0;
 const SPECULAR_HIGHLIGHT: u32 = 40;
 const SPECULAR_MULT: f32 = 0.25;
-const SUN_ENABLED: u32 = 1;
 const SUN_SHARPNESS: f32 = 2.0;
 const SUN_SIZE: f32 = 0.004;
 const VIGNETTE_STRENGTH: f32 = 0.5;
@@ -49,13 +68,21 @@ fn refraction(rd: vec3<f32>, n: vec3<f32>, p: f32) -> vec3<f32> {
 }
 
 fn planeFold(z: ptr<function, vec4<f32>>, n: vec3<f32>, d: f32) {
-    (*z).xyz -= 2.0 * min(0.0, dot((*z).xyz, n) - d) * n;
+    (*z) -= vec4(2.0 * min(0.0, dot((*z).xyz, n) - d) * n, 0);
 }
 
 fn sierpinskiFold(z: ptr<function, vec4<f32>>) {
-    (*z).xy -= min((*z).x + (*z).y, 0.0);
-    (*z).xz -= min((*z).x + (*z).z, 0.0);
-    (*z).yz -= min((*z).y + (*z).z, 0.0);
+    var d = min((*z).x + (*z).y, 0.0);
+    (*z).x -= d;
+    (*z).y -= d;
+
+    d = min((*z).x + (*z).z, 0.0);
+    (*z).x -= d;
+    (*z).z -= d;
+
+    d = min((*z).y + (*z).z, 0.0);
+    (*z).y -= d;
+    (*z).z -= d;
 }
 
 fn mengerFold(z: ptr<function, vec4<f32>>) {
@@ -71,30 +98,30 @@ fn mengerFold(z: ptr<function, vec4<f32>>) {
 }
 
 fn boxFold(z: ptr<function, vec4<f32>>, r: vec3<f32>) {
-    (*z).xyz = clamp((*z).xyz, -r, r) * 2.0 - (*z).xyz;
+    *z = vec4f(clamp((*z).xyz, -r, r) * 2.0 - (*z).xyz, (*z).w);
 }
 
 fn rotX(z: ptr<function, vec4<f32>>, s: f32, c: f32) {
-    (*z).yz = vec2<f32>(c * (*z).y + s * (*z).z, c * (*z).z - s * (*z).y);
+    *z = vec4f((*z).x, c * (*z).y + s * (*z).z, c * (*z).z - s * (*z).y, (*z).w);
 }
 
 fn rotY(z: ptr<function, vec4<f32>>, s: f32, c: f32) {
-    (*z).xz = vec2<f32>(c * (*z).x - s * (*z).z, c * (*z).z + s * (*z).x);
+    *z = vec4f(c * (*z).x - s * (*z).z, (*z).y,  c * (*z).z + s * (*z).x, (*z).w);
 }
 
 fn rotZ(z: ptr<function, vec4<f32>>, s: f32, c: f32) {
-    (*z).xy = vec2<f32>(c * (*z).x + s * (*z).y, c * (*z).y - s * (*z).x);
+    *z = vec4f(c * (*z).x + s * (*z).y, c * (*z).y - s * (*z).x, (*z).z, (*z).w);
 }
 
-fn rotX(z: ptr<function, vec4<f32>>, a: f32) {
+fn rotXAngle(z: ptr<function, vec4<f32>>, a: f32) {
     rotX(z, sin(a), cos(a));
 }
 
-fn rotY(z: ptr<function, vec4<f32>>, a: f32) {
+fn rotYAngle(z: ptr<function, vec4<f32>>, a: f32) {
     rotY(z, sin(a), cos(a));
 }
 
-fn rotZ(z: ptr<function, vec4<f32>>, a: f32) {
+fn rotZAngle(z: ptr<function, vec4<f32>>, a: f32) {
     rotZ(z, sin(a), cos(a));
 }
 
@@ -104,7 +131,7 @@ fn de_sphere(p: vec4<f32>, r: f32) -> f32 {
 
 fn de_box(p: vec4<f32>, s: vec3<f32>) -> f32 {
     let a = abs(p.xyz) - s;
-    return (min(max(max(a.x, a.y), a.z), 0.0) + length(max(a, 0.0))) / p.w;
+    return (min(max(max(a.x, a.y), a.z), 0.0) + length(max(a, vec3f(0)))) / p.w;
 }
 
 fn de_tetrahedron(p: vec4<f32>, r: f32) -> f32 {
@@ -117,51 +144,53 @@ fn de_capsule(p: vec4<f32>, h: f32, r: f32) -> f32 {
     return (length(vec3<f32>(p.x, py, p.z)) - r) / p.w;
 }
 
-fn de_fractal(p: vec4<f32>) -> f32 {
+fn de_fractal(point: vec4<f32>) -> f32 {
+    var p = point;
     for (var i: u32 = 0; i < FRACTAL_ITER; i = i + 1) {
-        p.xyz = abs(p.xyz);
-        rotZ(&p, uniforms.iFracAng1);
+        p = vec4f(abs(p.xyz), p.w);
+        rotZAngle(&p, iFracAng1);
         mengerFold(&p);
-        rotX(&p, uniforms.iFracAng2);
-        p *= uniforms.iFracScale;
-        p.xyz += uniforms.iFracShift;
+        rotXAngle(&p, iFracAng2);
+        p *= iFracScale;
+        p += vec4f(iFracShift, 0);
     }
     return de_box(p, vec3<f32>(6.0, 6.0, 6.0));
 }
 
-fn col_fractal(p: vec4<f32>) -> vec4<f32> {
+fn col_fractal(point: vec4<f32>) -> vec4<f32> {
+    var p = point;
     var orbit = vec3<f32>(0.0, 0.0, 0.0);
     for (var i: u32 = 0; i < FRACTAL_ITER; i = i + 1) {
-        p.xyz = abs(p.xyz);
-        rotZ(&p, uniforms.iFracAng1);
+        p = vec4f(abs(p.xyz), p.w);
+        rotZAngle(&p, iFracAng1);
         mengerFold(&p);
-        rotX(&p, uniforms.iFracAng2);
-        p *= uniforms.iFracScale;
-        p.xyz += uniforms.iFracShift;
-        orbit = max(orbit, p.xyz * uniforms.iFracCol);
+        rotXAngle(&p, iFracAng2);
+        p *= iFracScale;
+        p += vec4f(iFracShift, 0);
+        orbit = max(orbit, p.xyz * iFracCol);
     }
     return vec4<f32>(orbit, de_box(p, vec3<f32>(6.0, 6.0, 6.0)));
 }
 
 fn de_marble(p: vec4<f32>) -> f32 {
-    return de_sphere(p - vec4<f32>(uniforms.iMarblePos, 0.0), uniforms.iMarbleRad);
+    return de_sphere(p - vec4<f32>(iMarblePos, 0.0), iMarbleRad);
 }
 
 fn col_marble(p: vec4<f32>) -> vec4<f32> {
-    return vec4<f32>(0.0, 0.0, 0.0, de_sphere(p - vec4<f32>(uniforms.iMarblePos, 0.0), uniforms.iMarbleRad));
+    return vec4<f32>(0.0, 0.0, 0.0, de_sphere(p - vec4<f32>(iMarblePos, 0.0), iMarbleRad));
 }
 
 fn de_flag(p: vec4<f32>) -> f32 {
-    let f_pos = uniforms.iFlagPos + vec3<f32>(1.5, 4.0, 0.0) * uniforms.iFlagScale;
-    var d = de_box(p - vec4<f32>(f_pos, 0.0), vec3<f32>(1.5, 0.8, 0.08) * uniforms.iMarbleRad);
-    d = min(d, de_capsule(p - vec4<f32>(uniforms.iFlagPos + vec3<f32>(0.0, uniforms.iFlagScale * 2.4, 0.0), 0.0), uniforms.iMarbleRad * 2.4, uniforms.iMarbleRad * 0.18));
+    let f_pos = iFlagPos + vec3<f32>(1.5, 4.0, 0.0) * iFlagScale;
+    var d = de_box(p - vec4<f32>(f_pos, 0.0), vec3<f32>(1.5, 0.8, 0.08) * iMarbleRad);
+    d = min(d, de_capsule(p - vec4<f32>(iFlagPos + vec3<f32>(0.0, iFlagScale * 2.4, 0.0), 0.0), iMarbleRad * 2.4, iMarbleRad * 0.18));
     return d;
 }
 
 fn col_flag(p: vec4<f32>) -> vec4<f32> {
-    let f_pos = uniforms.iFlagPos + vec3<f32>(1.5, 4.0, 0.0) * uniforms.iFlagScale;
-    let d1 = de_box(p - vec4<f32>(f_pos, 0.0), vec3<f32>(1.5, 0.8, 0.08) * uniforms.iMarbleRad);
-    let d2 = de_capsule(p - vec4<f32>(uniforms.iFlagPos + vec3<f32>(0.0, uniforms.iFlagScale * 2.4, 0.0), 0.0), uniforms.iMarbleRad * 2.4, uniforms.iMarbleRad * 0.18);
+    let f_pos = iFlagPos + vec3<f32>(1.5, 4.0, 0.0) * iFlagScale;
+    let d1 = de_box(p - vec4<f32>(f_pos, 0.0), vec3<f32>(1.5, 0.8, 0.08) * iMarbleRad);
+    let d2 = de_capsule(p - vec4<f32>(iFlagPos + vec3<f32>(0.0, iFlagScale * 2.4, 0.0), 0.0), iMarbleRad * 2.4, iMarbleRad * 0.18);
     if (d1 < d2) {
         return vec4<f32>(1.0, 0.2, 0.1, d1);
     } else {
@@ -208,12 +237,12 @@ fn ray_march(p: ptr<function, vec4<f32>>, ray: vec4<f32>, sharpness: f32) -> vec
     var d = de_scene(*p);
     if (d < 0.0 && sharpness == 1.0) {
         var v: vec3<f32>;
-        if (abs(uniforms.iMarblePos.x) >= 999.0) {
-            v = (-20.0 * uniforms.iMarbleRad) * uniforms.iMat[2].xyz;
+        if (abs(iMarblePos.x) >= 999.0) {
+            v = (-20.0 * iMarbleRad) * iMat[2].xyz;
         } else {
-            v = uniforms.iMarblePos.xyz - uniforms.iMat[3].xyz;
+            v = iMarblePos.xyz - iMat[3].xyz;
         }
-        d = dot(v, v) / dot(v, ray.xyz) - uniforms.iMarbleRad;
+        d = dot(v, v) / dot(v, ray.xyz) - iMarbleRad;
     }
     var s = 0.0;
     var td = 0.0;
@@ -244,94 +273,75 @@ fn scene(p: ptr<function, vec4<f32>>, ray: ptr<function, vec4<f32>>, vignette: f
     let min_dist = max(FOVperPixel * td, MIN_DIST);
     if (d < min_dist) {
         let n = calcNormal(*p, min_dist * 0.5);
-        (*p).xyz -= n * d;
+        *p -= vec4f(n * d, 0);
 
-        #if FILTERING_ENABLE
-            let s1 = normalize(cross(ray.xyz, n));
-            let s2 = cross(s1, n);
-            let orig_col = clamp(smoothColor(*p, s1, s2, min_dist * 0.5), 0.0, 1.0);
-        #else
-            let orig_col = clamp(col_scene(*p), 0.0, 1.0);
-        #endif
+        let orig_col = saturate(col_scene(*p));
         col.w = orig_col.w;
 
         var k = 1.0;
-        #if SHADOWS_ENABLED
-            var light_pt = *p;
-            light_pt.xyz += n * MIN_DIST * 100.0;
-            let rm = ray_march(&light_pt, vec4<f32>(LIGHT_DIRECTION, 0.0), SHADOW_SHARPNESS);
-            k = rm.w * min(rm.z, 1.0);
-        #endif
+        var light_pt = *p;
+        light_pt += vec4f(n * MIN_DIST * 100.0, 0);
+        let rm = ray_march(&light_pt, vec4<f32>(LIGHT_DIRECTION, 0.0), SHADOW_SHARPNESS);
+        k = rm.w * min(rm.z, 1.0);
 
-        #if SPECULAR_HIGHLIGHT > 0
-            let reflected = ray.xyz - 2.0 * dot(ray.xyz, n) * n;
-            var specular = max(dot(reflected, LIGHT_DIRECTION), 0.0);
-            specular = pow(specular, f32(SPECULAR_HIGHLIGHT));
-            col.xyz += specular * LIGHT_COLOR * (k * SPECULAR_MULT);
-        #endif
+        let reflected = ray.xyz - 2.0 * dot(ray.xyz, n) * n;
+        var specular = max(dot(reflected, LIGHT_DIRECTION), 0.0);
+        specular = pow(specular, f32(SPECULAR_HIGHLIGHT));
+        col += vec4f(specular * LIGHT_COLOR * (k * SPECULAR_MULT), 0);
 
-        #if DIFFUSE_ENHANCED_ENABLED
-            k = min(k, SHADOW_DARKNESS * 0.5 * (dot(n, LIGHT_DIRECTION) - 1.0) + 1.0);
-        #elif DIFFUSE_ENABLED
-            k = min(k, dot(n, LIGHT_DIRECTION));
-        #endif
+        k = min(k, SHADOW_DARKNESS * 0.5 * (dot(n, LIGHT_DIRECTION) - 1.0) + 1.0);
 
         k = max(k, 1.0 - SHADOW_DARKNESS);
-        col.xyz += orig_col.xyz * LIGHT_COLOR * k;
+        col += vec4f(orig_col.xyz * LIGHT_COLOR * k, 0);
 
         let a = 1.0 / (1.0 + s * AMBIENT_OCCLUSION_STRENGTH);
-        col.xyz += (1.0 - a) * AMBIENT_OCCLUSION_COLOR_DELTA;
-
-        #if FOG_ENABLED
-            let a = td / MAX_DIST;
-            col.xyz = (1.0 - a) * col.xyz + a * BACKGROUND_COLOR;
-        #endif
+        col += vec4f((1.0 - a) * AMBIENT_OCCLUSION_COLOR_DELTA, 0);
 
         *ray = vec4<f32>(n, 0.0);
     } else {
-        col.xyz += BACKGROUND_COLOR;
-        col.xyz *= vignette;
-        #if SUN_ENABLED
-            let sun_spec = dot(ray.xyz, LIGHT_DIRECTION) - 1.0 + SUN_SIZE;
-            sun_spec = min(exp(sun_spec * SUN_SHARPNESS / SUN_SIZE), 1.0);
-            col.xyz += LIGHT_COLOR * sun_spec;
-        #endif
+        col += vec4f(BACKGROUND_COLOR, 0);
+        col *= vec4f(vec3f(vignette), 1);
+        
+        var sun_spec = dot(ray.xyz, LIGHT_DIRECTION) - 1.0 + SUN_SIZE;
+        sun_spec = min(exp(sun_spec * SUN_SHARPNESS / SUN_SIZE), 1.0);
+        col += vec4(LIGHT_COLOR * sun_spec, 0);
     }
 
     return col;
 }
 
 @fragment
-fn main(@builtin(position) fragCoord: vec4<f32>) -> @location(0) vec4<f32> {
-    FOVperPixel = 1.0 / max(uniforms.iResolution.x, 900.0);
+fn main(@builtin(position) origFragCoord: vec4<f32>) -> @location(0) vec4<f32> {
+    let fragCoord = vec4f(origFragCoord.x, -origFragCoord.y, origFragCoord.zw);
+    FOVperPixel = 1.0 / max(iResolution.x, 900.0);
 
     var col = vec3<f32>(0.0, 0.0, 0.0);
     for (var i: u32 = 0; i < ANTIALIASING_SAMPLES; i = i + 1) {
         for (var j: u32 = 0; j < ANTIALIASING_SAMPLES; j = j + 1) {
             let delta = vec2<f32>(f32(i), f32(j)) / f32(ANTIALIASING_SAMPLES);
-            let screen_pos = (fragCoord.xy + delta) / uniforms.iResolution.xy;
+            let screen_pos = (fragCoord.xy + delta) / iResolution.xy;
 
             var uv = 2.0 * screen_pos - 1.0;
-            uv.x *= uniforms.iResolution.x / uniforms.iResolution.y;
+            uv.x *= iResolution.x / iResolution.y;
 
-            var ray = uniforms.iMat * normalize(vec4<f32>(uv.x, uv.y, -FOCAL_DIST, 0.0));
-            var p = uniforms.iMat[3];
+            var ray = iMat * normalize(vec4<f32>(uv.x, uv.y, -FOCAL_DIST, 0.0));
+            var p = iMat[3];
 
             let vignette = 1.0 - VIGNETTE_STRENGTH * length(screen_pos - 0.5);
             let r = ray.xyz;
             let col_r = scene(&p, &ray, vignette);
 
             if (col_r.w > 0.5) {
-                let n = normalize(uniforms.iMarblePos - p.xyz);
+                let n = normalize(iMarblePos - p.xyz);
                 let q = refraction(r, n, 1.0 / 1.5);
-                let p2 = p.xyz + (dot(q, n) * 2.0 * uniforms.iMarbleRad) * q;
-                let n2 = normalize(p2 - uniforms.iMarblePos);
+                let p2 = p.xyz + (dot(q, n) * 2.0 * iMarbleRad) * q;
+                let n2 = normalize(p2 - iMarblePos);
                 let q2 = (dot(q, r) * 2.0) * q - r;
                 var p_temp = vec4<f32>(p2 + n2 * (MIN_DIST * 10.0), 1.0);
                 var r_temp = vec4<f32>(q2, 0.0);
                 let refr = scene(&p_temp, &r_temp, 0.8).xyz;
 
-                let n3 = normalize(p.xyz - uniforms.iMarblePos);
+                let n3 = normalize(p.xyz - iMarblePos);
                 let q3 = r - n3 * (2.0 * dot(r, n3));
                 p_temp = vec4<f32>(p.xyz + n3 * (MIN_DIST * 10.0), 1.0);
                 r_temp = vec4<f32>(q3, 0.0);
@@ -344,7 +354,7 @@ fn main(@builtin(position) fragCoord: vec4<f32>) -> @location(0) vec4<f32> {
         }
     }
 
-    col *= uniforms.iExposure / (f32(ANTIALIASING_SAMPLES) * f32(ANTIALIASING_SAMPLES));
+    col *= iExposure / (f32(ANTIALIASING_SAMPLES) * f32(ANTIALIASING_SAMPLES));
     return vec4<f32>(clamp(col, vec3<f32>(0.0, 0.0, 0.0), vec3<f32>(1.0, 1.0, 1.0)), 1.0);
 }
 
