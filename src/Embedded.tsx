87e328badb3ea1a50c88a90a1a902e8e1346a12b
Joe Grinstead <npm@mailforce.net>
2024-07-23 23:54:19 -0400

    Add error message

--- src/Embedded.tsx
+++ src/Embedded.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { AmbushGame, rethrowError } from "@grinstead/ambush";
+import { AmbushGame, E_GPU_NOSUPPORT, GPULoadError } from "@grinstead/ambush";
 import { Graphics } from "./Graphics.tsx";
 import { Game } from "./Game.tsx";
 import { createGameStore } from "./GameStore.ts";
@@ -8,7 +8,21 @@ export function Embedded() {
   const [store, setStore] = createGameStore();
 
   return (
-    <AmbushGame class="game" fallback={rethrowError}>
+    <AmbushGame
+      class="game"
+      fallback={(e) => {
+        if (e instanceof GPULoadError && e.code === E_GPU_NOSUPPORT) {
+          return (
+            <div>
+              Your Browser does not support WebGPU, consider{" "}
+              <a href="https://www.google.com/chrome/dr/download/">Chrome</a>
+            </div>
+          );
+        }
+
+        return <div>{String(e)}</div>;
+      }}
+    >
       <Graphics />
       <Game store={store} setStore={setStore} />
     </AmbushGame>
